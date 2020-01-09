1774f510466c62ab5afae85a61fce79406a6e6de
Flo <flofds1@gmail.com>
2020-01-09 16:21:46 +0100

    Viele Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _counter = 0;
-
-  void _incrementCounter(BuildContext context) {
-    setState(() {
-      _counter++;
-    });
-    final snackBar = SnackBar(
-      content: Text('Du hast eine Zigarette mehr geraucht'),
-    );
-    Scaffold.of(context).showSnackBar(snackBar);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,38 +19,87 @@ class _MyAppState extends State<MyApp> {
           builder: (BuildContext context) {
             return Column(
               children: <Widget>[
-                FlatButton(
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.smoking_rooms),
-                      Text('        Zigaretten        '),
-                      Text('$_counter        ')
-                    ],
-                  ),
-                  onPressed: () => _incrementCounter(context),
+                CountButton(
+                  name: 'Bier',
+                  message: 'Du hast wieder Bier getrunken',
+                  icon: Icon(Icons.local_bar),
+                  color: Colors.limeAccent[400],
                 ),
+                CountButton(
+                  name: 'Zigarette',
+                  message: 'Du warst wieder Rauchen',
+                  icon: Icon(Icons.smoking_rooms),
+                  color: Colors.limeAccent[400],
+                ),
+                CountButton(
+                    name: 'Kaffee',
+                    message: 'Du trinkst schon wieder Kaffee?!',
+                    icon: Icon(Icons.local_cafe),
+                    color: Colors.limeAccent[400]),
+                CountButton(
+                  name: 'Wie oft warst du am Handy',
+                  message: 'Du bist schon wieder am Handy?!',
+                  icon: Icon(Icons.smartphone),
+                  color: Colors.limeAccent[400],
+                )
               ],
             );
           },
         ),
-        body: Builder(
-          builder: (BuildContext context) {
-            return Column(
-              children: <Widget>[
-                FlatButton(
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.smoking_rooms),
-                      Text('        Zigaretten        '),
-                      Text('$_counter        ')
-                    ],
-                  ),
-                  onPressed: () => _incrementCounter(context),
-                ),
-              ],
-            );
-          },
+      ),
+    );
+  }
+}
+
+class CountButton extends StatefulWidget {
+  const CountButton({
+    Key key,
+    @required this.name,
+    @required this.message,
+    @required this.icon,
+    @required this.color,
+  }) : super(key: key);
+
+  final String name;
+  final String message;
+  final Icon icon;
+  final Color color;
+
+  @override
+  _CountButtonState createState() => _CountButtonState();
+}
+
+class _CountButtonState extends State<CountButton> {
+  int _counter = 0;
+
+  void _incrementCounter(BuildContext context) {
+    setState(() {
+      _counter++;
+    });
+
+    final snackBar = SnackBar(
+      content: Text(widget.message),
+    );
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
+      child: FlatButton(
+        color: Colors.limeAccent[400],
+        child: Padding(
+          padding: EdgeInsets.only(top: 30.0, bottom: 30.0),
+          child: Row(
+            children: <Widget>[
+              widget.icon,
+              Text('        ${widget.name}        '),
+              Text('$_counter        '),
+            ],
+          ),
         ),
+        onPressed: () => _incrementCounter(context),
       ),
     );
   }
