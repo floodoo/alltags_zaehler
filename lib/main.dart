31a8e264d1e0bb81e16fb99d03f4e4af98766135
Flo <flofds1@gmail.com>
2020-01-14 15:55:32 +0100

    Man kann scrollen

--- lib/main.dart
+++ lib/main.dart
@@ -25,53 +25,79 @@ class MyCounter extends StatefulWidget {
 class _MyAppState extends State<MyCounter> {
   @override
   Widget build(BuildContext context) {
-    //final counterState = Provider.of<CounterState>(context);
+    List<CountButton> buttons = [
+      CountButton(
+        name: 'Bier',
+        message: 'Du hast schon wieder Bier getrunken?!',
+        icon: Icon(Icons.local_drink),
+        color: Colors.limeAccent[400],
+      ),
+      CountButton(
+        name: 'Zigarette',
+        message: 'Du warst wieder Rauchen?!',
+        icon: Icon(Icons.smoking_rooms),
+        color: Colors.limeAccent[400],
+      ),
+      CountButton(
+          name: 'Kaffee',
+          message: 'Du trinkst schon wieder Kaffee?!',
+          icon: Icon(Icons.local_cafe),
+          color: Colors.limeAccent[400]),
+      CountButton(
+        name: 'Handy',
+        message: 'Du bist schon wieder am Handy?!',
+        icon: Icon(Icons.smartphone),
+        color: Colors.limeAccent[400],
+      ),
+      CountButton(
+        name: 'Sport',
+        message: 'Weiter so!',
+        icon: Icon(Icons.directions_run),
+        color: Colors.limeAccent[400],
+      ),
+      CountButton(
+        name: 'Auto fahren',
+        message: 'Es wäre gut mal mehr zu laufen!',
+        icon: Icon(Icons.directions_car),
+        color: Colors.limeAccent[400],
+      ),
+      CountButton(
+        name: 'Ungesundes Essen',
+        message: 'Ess mal lieber was gesundes!',
+        icon: Icon(Icons.fastfood),
+        color: Colors.limeAccent[400],
+      ),
+      CountButton(
+        name: 'Gesundes Essen',
+        message: 'Weiter so!',
+        icon: Icon(Icons.local_dining),
+        color: Colors.limeAccent[400],
+      ),
+      CountButton(
+        name: 'Treppen laufen',
+        message: 'Weiter so',
+        icon: Icon(Icons.directions_run),
+        color: Colors.limeAccent[400],
+      ),
+    ];
 
     return Scaffold(
       appBar:
           AppBar(backgroundColor: Colors.orange, title: Text('Alltagszähler')),
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.add),
         backgroundColor: Colors.orange,
       ),
-      body: Builder(
-        builder: (BuildContext context) {
-          return Column(
-            children: <Widget>[
-              CountButton(
-                name: 'Bier',
-                message: 'Du hast wieder Bier getrunken',
-                icon: Icon(Icons.local_drink),
-                color: Colors.white,
-              ),
-              CountButton(
-                name: 'Zigarette',
-                message: 'Du warst wieder Rauchen',
-                icon: Icon(Icons.smoking_rooms),
-                color: Colors.white,
-              ),
-              CountButton(
-                  name: 'Kaffee',
-                  message: 'Du trinkst schon wieder Kaffee?!',
-                  icon: Icon(Icons.local_cafe),
-                  color: Colors.white),
-              CountButton(
-                name: 'Handy',
-                message: 'Du bist schon wieder am Handy?!',
-                icon: Icon(Icons.smartphone),
-                color: Colors.white,
-              ),
-              CountButton(
-                name: 'Sport',
-                message: 'Weiter so',
-                icon: Icon(Icons.directions_run),
-                color: Colors.white,
-              ),
-            ],
-          );
-        },
+      body: Container(
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: buttons.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(child: buttons[index]);
+          },
+        ),
       ),
     );
   }
